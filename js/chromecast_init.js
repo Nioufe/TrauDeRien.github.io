34f477ffa07e01661fea77abb5f791df68b4972e
adrien TRAUTH <adrientn@gmail.com>
2014-05-02 19:38:03 +0200

    ajout receiver sender

--- js/chromecast_init.js
+++ js/chromecast_init.js
@@ -14,13 +14,16 @@ window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
 }
 
 initializeCastApi = function() {
-
+  console.log('initCastApi');
   var sessionRequest = new chrome.cast.SessionRequest(applicationID);
 
   var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
     sessionListener,
     receiverListener);
   chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+  document.getElementById('init').addEventListener('click', function(){
+    initSession();
+  })
 };
 
 //verifie que des devices sont disponibles
@@ -36,6 +39,7 @@ function sessionListener(e) {
   }
 }
 function initSession() {
+  console.log('initSession');
   chrome.cast.requestSession(onRequestSessionSuccess, onLaunchError);
 }
 
